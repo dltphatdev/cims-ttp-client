65d84ab8e03d3a1d741eb20ee17c132f08b67ee6
thanhphatdev301 <dltphat301@gmail.com>
2025-07-09 01:31:24 +0000

    update src code 9.7.2025

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,21 @@ import path from 'path'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000
+    host: '0.0.0.0', // Cho phép truy cập từ bên ngoài
+    port: 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['cims.ttpgroupvn.com', 'localhost', '172.38.11.200'], // Thêm các host được phép
   },
   css: {
-    devSourcemap: true
+    devSourcemap: true,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, 'src/components')
-    }
-  }
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
 })
